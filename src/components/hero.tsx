0845182e7a0ce4941d96ddec6f4c674ce660d65e
ajay-dv8 <ajaydives3@gmail.com>
2025-06-15 21:20:33 +0000

    add gradient to hero title

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -8,14 +8,16 @@ const HeroBg = () => {
     <div id="home" className="h-svh w-svw">
       <ChromeGrid />
       <div className="absolute z-10 left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 pointer-events-none flex flex-col justify-center items-center text-center w-full md:w-4xl lg:w-2/3">
+      {/* title */}
         <motion.h1
-          className="text-4xl md:text-6xl font-normal mb-4 tracking-widest text-white"
+          className="text-4xl md:text-6xl font-normal mb-4 tracking-widest bg-gradient-to-b from-white to-gray-400 bg-clip-text text-transparent hover:from-white hover:to-gray-400 transition-all duration-300"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 1.2, delay: 0.3, ease: "easeInOut" }}
         >
           Transforming Ideas Into Digital Excellence
         </motion.h1>
+        {/* sub title */}
         <motion.p
           className="text-sm w-full md:w-xl md:text-base text-white/70 font-mono tracking-wide pointer-events-none"
           initial={{ opacity: 0 }}
