62bf6abe5606c748a922e16c32d04af50b928c88
ajay-dv8 <ajaydives3@gmail.com>
2025-06-15 20:11:24 +0000

    removed images from testimonials

--- src/components/ui/testmnial-columns.tsx
+++ src/components/ui/testmnial-columns.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import { motion } from "motion/react";
-import Image, { StaticImageData } from "next/image";
+import { StaticImageData } from "next/image";
 
 interface Testimonial {
   text: string;
@@ -32,17 +32,17 @@ export const TestimonialsColumn = (props: {
         {[
           ...new Array(2).fill(0).map((_, index) => (
             <React.Fragment key={index}>
-              {props.testimonials.map(({ text, image, name, role }, i) => (
+              {props.testimonials.map(({ text, name, role }, i) => (
                 <div className="p-6 md:p-10 rounded-3xl border max-w-xs w-full" key={i}>
                   <div className="text-gray-700">{text}</div>
                   <div className="flex items-center gap-2 mt-5">
-                    <Image
+                    {/* <Image
                       width={40}
                       height={40}
                       src={image ?? ''}
                       alt={name}
                       className="h-10 w-10 rounded-full"
-                    />
+                    /> */}
                     <div className="flex flex-col">
                       <div className="font-medium text-gray-800 tracking-tight leading-5">{name}</div>
                       <div className="leading-5 text-gray-600 opacity-60 tracking-tight">{role}</div>
